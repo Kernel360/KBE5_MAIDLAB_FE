1e0735fc928e070331be2768258b265ef2714161
JINS-DE <rlawlstjd044@naver.com>
2025-06-13 11:42:51 +0900

    feat : 예약 생성페이지 width 조절

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -3,8 +3,7 @@ import axios, { type AxiosInstance, type AxiosResponse } from 'axios';
 import { tokenStorage } from '@/utils/storage';
 
 const BASE_URL =
-  // import.meta.env.VITE_API_BASE_URL || 'https://api-maidlab.duckdns.org';
-  'http://localhost:8080';
+  import.meta.env.VITE_API_BASE_URL || 'https://api-maidlab.duckdns.org';
 
 // API 에러 코드 매핑 (백엔드와 동일)
 const API_CODE_MESSAGES = {
