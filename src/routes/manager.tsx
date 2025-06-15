7607085a51bf02923ecb3fa6f439ec5c40eb00c3
JINS-DE <rlawlstjd044@naver.com>
2025-06-15 15:18:54 +0900

    feat : 매니저 정산 페이지 구현

--- src/routes/manager.tsx
+++ src/routes/manager.tsx
@@ -6,6 +6,7 @@ import ManagerReservations from '@/pages/reservation/ManagerReservations';
 import ManagerReservationDetail from '@/pages/reservation/ManagerReservationDetail';
 import ManagerReviewRegister from '@/pages/manager/ReviewRegister';
 import ManagerMatching from '@/pages/matching/ManagerMatching';
+import ManagerSettlements from '@/pages/manager/ManagerSettlements';
 
 export const ManagerRoutes = () => (
   <>
@@ -46,6 +47,14 @@ export const ManagerRoutes = () => (
       element={
         <ProtectedRoute requiredUserType="MANAGER">
           <ManagerMatching />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path={ROUTES.MANAGER.SETTLEMENTS}
+      element={
+        <ProtectedRoute requiredUserType="MANAGER">
+          <ManagerSettlements />
         </ProtectedRoute>
       }
     />
