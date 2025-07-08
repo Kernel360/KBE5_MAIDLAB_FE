94480ce260d9db7cc6f2acd245e39c0bfdceb790
soooo-ning <ssoning13@gmail.com>
2025-07-08 14:37:42 +0900

    refactor: admin 페이지 뷰포트 적용 안되게 조건부 렌더링 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, useLocation } from 'react-router-dom';
 import App from './App';
 import { ScrollToTop } from './components/common';
 import { DesktopViewport } from './components/layout';
@@ -17,22 +17,51 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
+// 조건부 레이아웃 컴포넌트
+const ConditionalLayout = () => {
+  const location = useLocation();
+  const isAdminRoute = location.pathname.startsWith('/admin');
+
+  if (isAdminRoute) {
+    // Admin 페이지는 전체 화면으로 렌더링
+    return (
+      <>
+        <ScrollToTop />
+        <App />
+        
+        {/* 토스트 컨테이너 - Admin에서도 표시 */}
+        <div style={{ position: 'fixed', zIndex: 10000 }}>
+          <ToastContainer />
+        </div>
+        <Analytics />
+      </>
+    );
+  }
+
+  // 일반 페이지는 뷰포트로 렌더링
+  return (
+    <>
+      <ScrollToTop />
+      <DesktopViewport>
+        <App />
+      </DesktopViewport>
+      
+      {/* 토스트 컨테이너 - 최상위에서 관리 */}
+      <div style={{ position: 'fixed', zIndex: 10000 }}>
+        <ToastContainer />
+      </div>
+      <Analytics />
+    </>
+  );
+};
+
 const SimpleViewportResize = () => {
   return (
     <ThemeProvider>
       <ToastProvider>
         <BrowserRouter>
           <AuthProvider>
-            <ScrollToTop />
-            <DesktopViewport>
-              <App />
-            </DesktopViewport>
-            
-            {/* 토스트 컨테이너 - 최상위에서 관리 */}
-            <div style={{ position: 'fixed', zIndex: 10000 }}>
-              <ToastContainer />
-            </div>
-            <Analytics />
+            <ConditionalLayout />
           </AuthProvider>
         </BrowserRouter>
       </ToastProvider>
